13e2680592729d01e989451f52009616205aff19
JailsonA <maurocv18@gmail.com>
2024-01-17 17:12:48 +0000

    Extrutura base da DAL setado

---
+++ DAL_ExpenseManagement/DAL_ExpenseManagement/Services/Implementations/UserServices.cs
@@ -0,0 +1,13 @@
+﻿using DAL_ExpenseManagement.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_ExpenseManagement.Services.Implementations
+{
+    public class UserServices : IUserServices
+    {
+    }
+}
